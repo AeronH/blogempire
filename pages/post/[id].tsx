4600ae5462803b1372dc7d3c7168bf5f905a98ae
AeronH <aeronhorne@gmail.com>
2022-07-21 19:40:56 -0500

    Update [id].tsx

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -17,72 +17,74 @@ interface Props {
 
 function Post({post, authorPosts}: Props) {
   
-  return (
-    <main className='bg-slate-100 min-h-screen'>
-      <Head>
-        <title>{post.title}</title>
-      </Head>
-      <Header/>
-      
-      <section className='max-w-7xl mx-auto p-10 flex'>
-        <div className=' px-2 w-full lg:w-3/4 sm:px-10'>
+  if(post) {
+    return (
+      <main className='bg-slate-100 min-h-screen'>
+        <Head>
+          <title>{post?.title}</title>
+        </Head>
+        <Header/>
+        
+        <section className='max-w-7xl mx-auto p-10 flex'>
+          <div className=' px-2 w-full lg:w-3/4 sm:px-10'>
 
-          <div className='flex flex-col items-center pt-10'>
-            <div className='space-y-5 flex flex-col items-center'>
-              <h1 className='text-4xl'>{post.title}</h1>
-              <h1 className='text-xl opacity-70'>{post.description}</h1>
-              <Link href={`/author/${post.author.uid}`}>
-                <div className='flex space-x-1 items-center cursor-pointer'>
-                  <p>By: </p>
-                  <Avatar 
-                    src={post.author.image}
-                    size='xs'
-                    name={post.author.name}/>
-                  <p>{post.author.name}</p>
-                </div>
-              </Link>
-              <h2 className='text-slate-500'>{post.publishedDate}</h2>
+            <div className='flex flex-col items-center pt-10'>
+              <div className='space-y-5 flex flex-col items-center'>
+                <h1 className='text-4xl'>{post.title}</h1>
+                <h1 className='text-xl opacity-70'>{post.description}</h1>
+                <Link href={`/author/${post.author.uid}`}>
+                  <div className='flex space-x-1 items-center cursor-pointer'>
+                    <p>By: </p>
+                    <Avatar 
+                      src={post.author.image}
+                      size='xs'
+                      name={post.author.name}/>
+                    <p>{post.author.name}</p>
+                  </div>
+                </Link>
+                <h2 className='text-slate-500'>{post.publishedDate}</h2>
+              </div>
+
+              {post.mainImage && <div className='flex justify-center h-72 w-full mt-10 px-10 overflow-hidden'>
+                                  <img
+                                    className='w-full object-contain'
+                                    src={post.mainImage}
+                                  />
+                                </div>}
+
+              <div className='text-xl mt-10'>
+                <p className='indent-10'>{post.body}</p>
+              </div>
+              
             </div>
-
-            {post.mainImage && <div className='flex justify-center h-72 w-full mt-10 px-10 overflow-hidden'>
-                                 <img
-                                   className='w-full object-contain'
-                                   src={post.mainImage}
-                                 />
-                               </div>}
-
-            <div className='text-xl mt-10'>
-              <p className='indent-10'>{post.body}</p>
-            </div>
-            
+            <Comments post={post}/>
           </div>
-          <Comments post={post}/>
-        </div>
 
-        <div className='hidden lg:inline-block sticky top-32 w-1/4 p-4 h-fit'>
-          <div className='flex flex-col justify-start'>
-            <img className='h-40 w-40 rounded-full object-cover' src={post.author.image} alt="" />
-            <h1 className='text-2xl mt-2'>{post.author.name}</h1>
-            <div className='mt-3 flex flex-wrap'>
-              <Link href={`/author/${post.author.uid}`}>
-                <button className=' text-slate-800 border border-slate-800 px-3 py-1 w-fit h-fit mt-2 hover:bg-slate-800 hover:text-white transition ease-in-out duration-300'>View Profile</button>
-              </Link>
-            </div>
+          <div className='hidden lg:inline-block sticky top-32 w-1/4 p-4 h-fit'>
+            <div className='flex flex-col justify-start'>
+              <img className='h-40 w-40 rounded-full object-cover' src={post.author.image} alt="" />
+              <h1 className='text-2xl mt-2'>{post.author.name}</h1>
+              <div className='mt-3 flex flex-wrap'>
+                <Link href={`/author/${post.author.uid}`}>
+                  <button className=' text-slate-800 border border-slate-800 px-3 py-1 w-fit h-fit mt-2 hover:bg-slate-800 hover:text-white transition ease-in-out duration-300'>View Profile</button>
+                </Link>
+              </div>
 
-            <div className='mt-10'>
-              <h1 className='text-xl'>More from {post.author.name}:</h1>
-              <div className='h-fit space-y-3 mt-5 p-3'>
-                {authorPosts.map(post => (
-                  <SidebarPostCard key={post.id} post={post}/>
-                ))}
+              <div className='mt-10'>
+                <h1 className='text-xl'>More from {post.author.name}:</h1>
+                <div className='h-fit space-y-3 mt-5 p-3'>
+                  {authorPosts.map(post => (
+                    <SidebarPostCard key={post.id} post={post}/>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <Footer />
-    </main>
-  )
+        </section>
+        <Footer />
+      </main>
+    )
+  }
 }
 
 export default Post
